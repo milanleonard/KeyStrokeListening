b3a9ff12b87588835fed8e94fef8ba21a7d9337f
Milan Leonard <1357619+milanleonard@users.noreply.github.com>
2019-11-14 09:54:30 +1100

    Data generater, finished

--- generate_data.py
+++ generate_data.py
@@ -1,38 +1,61 @@
 import pyaudio
 import wave
+import os
+import time
+from pynput.keyboard import Key, Listener
+import logging
+import multiprocessing
+import argparse
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "voice.wav"
+def keypress(Key):
+	logging.info(str(Key))
 
-p = pyaudio.PyAudio()
+def keylogger(num_seconds):
+	log_directory = ""
+	logging.basicConfig(filename ="./data/log_results.txt",level = logging.DEBUG, format = '%(asctime)s : %(message)s')
+	listener = Listener(
+    on_press=keypress)
+	return listener
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+def begin_audio(num_seconds, CHUNK=1024,CHANNELS=2,RATE=44100,FORMAT = pyaudio.paInt16):
+	FORMAT = pyaudio.paInt16
+	RECORD_SECONDS = num_seconds
+	WAVE_OUTPUT_FILENAME = "data.wav"
+	p = pyaudio.PyAudio()
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK)
+	print("* recording")
+	frames = []
+	listener = keylogger(num_seconds)
+	listener.start()
+	start = time.time()
+	while time.time()-start < num_seconds:
+		print(time.time()-start)
+		for i in range(0, int(RATE / CHUNK * num_seconds)):
+		    data = stream.read(CHUNK)
+		    frames.append(data)
+		print("* done recording")
+		listener.stop()
+		stream.stop_stream()
+		stream.close()
+		wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+		wf.setnchannels(CHANNELS)
+		wf.setsampwidth(p.get_sample_size(FORMAT))
+		wf.setframerate(RATE)
+		wf.writeframes(b''.join(frames))
+		wf.close()
+def make_dirs():
+	if not os.path.exists("./data"):
+		os.makedirs("./data")
 
-print("* recording")
-
-frames = []
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print("* done recording")
-
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Log keypresses and record sound')
+	parser.add_argument('--seconds',help='How many seconds',type=int,default=1)
+	args = parser.parse_args()
+	num_seconds = args.seconds
+	make_dirs()
+	begin_audio(num_seconds)
+	
